11828f59821ae58253d1174c50bf0d782265933f
Andy <rsaman@powernerdsoftware.com>
2017-11-24 16:54:02 -0800

    Added ajax-post boilerplate
    This ajax-poast boilerplate example came from the codecademy.com course Introduction To JavaScript: Requests I.

---
+++ ajax-post.js
@@ -0,0 +1,11 @@
+const xhr = new XMLHttpRequest();
+const url = 'https://api-to-call.com/endpoint';
+const data = JSON.stringify({id: '200'});
+xhr.responseType = 'json';
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    console.log(xhr.response);
+  }
+}
+xhr.open('POST', url);
+xhr.send(data);
